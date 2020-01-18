142fb9f4c817bc1085e73ca3aab667298ebac09a
realblingy <jamesphidang@gmail.com>
2020-01-18 14:10:50 +1100

    Added flask_login but may need fixing

--- eggy.py
+++ eggy.py
@@ -7,6 +7,8 @@ from wtforms import StringField, PasswordField, BooleanField
 from wtforms.validators import InputRequired, Email, Length
 from flask_sqlalchemy import SQLAlchemy
 import os
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'THISISASOCIETY'
@@ -14,12 +16,19 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 app.config['SQLAlCHEMY_TRACK_MODIFICATIONS'] = False
 Bootstrap(app)
 db = SQLAlchemy(app)
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(15), unique=True)
     email = db.Column(db.String(50), unique=True)
     password = db.Column(db.String(80))
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired(), Length(min=4, max=15)])
@@ -94,6 +103,7 @@ def index():
         return render_template('notfound.html')
 
 @app.route('/about/')
+@login_required
 def about():
     items = [dict(name = 'Nintendo Switch', price = 499.99, store = 'JB HI FI')]
     return render_template('about.html', items=items, quantity=len(items))
@@ -103,7 +113,7 @@ def signup():
     form = RegisterForm()
 
     if form.validate_on_submit():
-        new_user = User(username=form.username.data, email=form.email.data, password=form.password.data)
+        new_user = User(username=form.username.data, email=form.email.data, password=generate_password_hash(form.password.data, method='sha256'))
         db.session.add(new_user)
         db.session.commit()
 
@@ -116,7 +126,8 @@ def login():
     form = LoginForm()
     user = User.query.filter_by(username=form.username.data).first()
     if user:
-        if user.password == form.password.data:
+        if check_password_hash(user.password, form.password.data):
+            login_user(user, remember=form.remember.data)
             return redirect(url_for('index'))
 
         return '<h1>Invalid username of password</h1>'
